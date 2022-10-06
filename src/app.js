d237311873d4847ed83eb0680f0018e916fdafeb
yasRF <yas070788@hotmail.es>
2022-10-06 18:46:05 +0000

    mi primer commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,42 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  window.genCard = function() {
+    const suit = ["♥", "♠", "♣", "♦"];
+    const numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    function randomCard() {
+      let randomSuit = suit[Math.floor(Math.random() * suit.length)];
+      let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+      document.querySelector(".icono").innerHTML = randomSuit;
+      document.querySelector(".cardContentContainer").innerHTML = randomNumber;
+      document.querySelector(".icono2").innerHTML = randomSuit;
+
+      if (randomSuit == "♥" || randomSuit == "♦") {
+        document.querySelector("#cardcontainer").style.color = "red";
+      } else if (randomSuit == "♠" || randomSuit == "♣") {
+        document.querySelector("#cardcontainer").style.color = "black";
+      }
+    }
+
+    randomCard();
+  };
+
+  window.genCard();
 };
